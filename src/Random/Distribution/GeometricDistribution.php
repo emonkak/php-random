2dceabd5d7cffca150430cf16d11909ea17aa618
Shota Nozaki <emonkak@gmail.com>
2013-11-07 22:40:14 +0900

    Implement GeometricDistribution

---
+++ src/Random/Distribution/GeometricDistribution.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This file is part of the Random.php package.
+ *
+ * Copyright (C) 2013 Shota Nozaki <emonkak@gmail.com>
+ *
+ * Licensed under the MIT License
+ */
+
+namespace Random\Distribution;
+
+use Random\Engine\AbstractEngine;
+
+class GeometricDistribution extends AbstractDistribution
+{
+    /**
+     * @var float
+     */
+    private $probability;
+
+    /**
+     * @param float $probability
+     */
+    public function __construct($probability)
+    {
+        assert(0 < $probability && $probability < 1);
+
+        $this->probability = $probability;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function generate(AbstractEngine $engine)
+    {
+        $result = log(1 - $engine->nextDouble()) / log(1 - $this->probability);
+
+        return (int) $result;
+    }
+}
