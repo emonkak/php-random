aaa9dbf4e8c0766e12299e45809e053e0d739a6c
Shota Nozaki <emonkak@gmail.com>
2019-07-23 18:06:28 +0900

    RandomDevice: Fix for PHP 7.x

--- src/Engine/RandomDevice.php
+++ src/Engine/RandomDevice.php
@@ -25,7 +25,7 @@ class RandomDevice extends AbstractEngine
      */
     public function next()
     {
-        $bytes = mcrypt_create_iv(4, MCRYPT_DEV_URANDOM);
+        $bytes = function_exists('random_bytes') ? random_bytes(4) : mcrypt_create_iv(4, MCRYPT_DEV_URANDOM);
         if ($bytes === false || strlen($bytes) !== 4) {
             throw new RuntimeException('Unable to get 4 bytes');
         }
